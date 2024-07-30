318c5a2691303e20f41f9251e05f51cd54056286
olasunkanmi <olasunkanmiologun64@email.com>
2024-07-30 16:03:56 +0100

    added api key to firebase config

--- app/firebase/config.ts
+++ app/firebase/config.ts
@@ -3,12 +3,12 @@ import {getAuth} from 'firebase/auth';
 import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+    apiKey:"AIzaSyCxImfAh2O82ID7MlR4S3BthxKT1uwSbcw",
+    authDomain:"testly-92aeb.firebaseapp.com",
+    projectId:"testly-92aeb",
+    storageBucket:"testly-92aeb.appspot.com",
+    messagingSenderId:"357578171316",
+    appId:"1:357578171316:web:657f6d9e51dea97380c8e1"
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
